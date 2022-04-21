dcc542bbe79ff1699aaa5900aa1d657503eb7d08
opencourse5tutor <opencourse5tutor@codl.lk>
2022-04-21 14:18:48 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-// run `node index.js` in the terminal
+var fs = require('fs');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const content = 'This is the content which will write to text file!'
+
+fs.writeFileSync('test.txt', content, function (result, err) {
+   if (err) return console.error(err);
+   console.log(result.toString());
+});
